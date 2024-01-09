2552e8eb56e909214a9d28973fdea2c1df9b3900
Rhythmgulati <109156350+Rhythmgulati@users.noreply.github.com>
2024-01-10 00:06:02 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ const imgcontainer = document.querySelector(".imgcontainer");
 const qrtext = document.querySelector("#inputtext");
 
 function generator() {
+    if(qrtext.value.length>0){
     image.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrtext.value}`;
     imgcontainer.classList.add("show-img")
+    }else{   qrtext.classList.add("error");
+        setTimeout(()=>{
+            qrtext.classList.remove("error");
+        },1000)
+    }
+
 }
